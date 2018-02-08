594e10263110bbbd03c486b50d608277195f8407
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-02-09 00:01:14 +0900

    Lottery

---
+++ Lottery/Lottery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by Tatsuo Ikeda on 2018/02/08.
+//  Copyright © 2018 Tatsuo Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
