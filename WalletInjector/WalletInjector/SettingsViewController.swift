1a93a8dcf56a97e1e7fd3a9ffa0c8adcda770c3f
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-02-16 00:28:17 +0900

    chart

--- WalletInjector/WalletInjector/SettingsViewController.swift
+++ WalletInjector/WalletInjector/SettingsViewController.swift
@@ -7,29 +7,64 @@
 //
 
 import UIKit
+import Charts
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, ChartViewDelegate {
 
+    @IBOutlet var lineChartView: LineChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setupLineChartView()
+        
+        setupData()
     }
 
+    func setupLineChartView(){
+        let chartView: LineChartView = LineChartView()
+            chartView.delegate = self
+            chartView.frame = view.frame
+        view.addSubview(chartView)
+        
+        lineChartView = chartView
+    }
+    
+    
+    func setupData(){
+        
+        var lineChartEntry  = [ChartDataEntry]()
+        for i in 1...20 {
+            let chartData = ChartDataEntry(x: Double(i), y: Double(arc4random_uniform(UInt32(i))*2))
+            lineChartEntry.append(chartData)
+        }
+        
+        // Colors of the gradient
+        let gradientColors = [UIColor.blue.cgColor, UIColor.clear.cgColor] as CFArray
+        // Positioning of the gradient
+        let colorLocations:[CGFloat] = [1.0, 0.0]
+        let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(),
+                                       colors: gradientColors,
+                                       locations: colorLocations)
+        
+        let line = LineChartDataSet(values: lineChartEntry, label: "test")
+        // line.colors = [NSUIColor.black]
+        line.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0)
+        line.drawFilledEnabled = true
+        line.mode = .horizontalBezier
+        
+        let data = LineChartData()
+        data.addDataSet(line)
+        
+        lineChartView.data = data
+        lineChartView.chartDescription?.text = "Hello!"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
