f9ec1b9b1b5771510fc0940a654db2853cc0190d
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-03-18 23:21:17 +0900

    design

---
+++ PracticeDesign/PracticeDesign/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  PracticeDesign
+//
+//  Created by Tatsuo Ikeda on 2018/03/18.
+//  Copyright © 2018 Tatsuo Ikeda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        if let initialTab = self.window!.rootViewController as? UITabBarController {
+            initialTab.selectedIndex = 2
+        }
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) { }
+
+    func applicationDidEnterBackground(_ application: UIApplication) { }
+
+    func applicationWillEnterForeground(_ application: UIApplication) { }
+
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+
+    func applicationWillTerminate(_ application: UIApplication) { }
+
+
+}
+
