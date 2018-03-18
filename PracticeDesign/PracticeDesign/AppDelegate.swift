c9d962c324901e25b35d27f3bfece0056b77a18c
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-03-19 00:07:18 +0900

    design

--- PracticeDesign/PracticeDesign/AppDelegate.swift
+++ PracticeDesign/PracticeDesign/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         if let initialTab = self.window!.rootViewController as? UITabBarController {
-            initialTab.selectedIndex = 2
+            initialTab.selectedIndex = 0
         }
 
         return true
