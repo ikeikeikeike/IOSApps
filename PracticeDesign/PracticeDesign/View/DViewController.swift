9586e593f2782292672a15a5116f05413dc4fb51
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-03-22 05:04:19 +0900

    table view with enum

--- PracticeDesign/PracticeDesign/View/DViewController.swift
+++ PracticeDesign/PracticeDesign/View/DViewController.swift
@@ -11,10 +11,30 @@ import UIKit
 class DViewController: UITableViewController {
     
     let cellID = "DTableViewCell"
-    let sections = [
-        ["baka", "unko"],
-        ["baka", "unko"],
-    ]
+    
+    enum Section: Int {
+        case User = 0
+        case Account
+        
+        static let count: Int = Account.rawValue + 1
+        
+        func rows() -> [String] {
+            switch self {
+            case .User:
+                return ["baka", "unko"]
+            case .Account:
+                return ["chinko", "manko"]
+            }
+        }
+
+        func rowsCount() -> Int {
+            return rows().count
+        }
+        
+        subscript(index: Int) -> String {
+            return rows()[index]
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,18 +55,26 @@ class DViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return sections.count
+        return Section.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return sections[section].count
+        
+        switch Section(rawValue: section) {
+        case .some(.User):
+            return Section.User.rowsCount()
+        case .some(.Account):
+            return Section.Account.rowsCount()
+        default:
+            return 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
 
-        cell.textLabel?.text = sections[indexPath.section][indexPath.row]
+        cell.textLabel?.text = Section(rawValue: indexPath.section)?[indexPath.row]
         // Configure the cell...
 
         return cell
@@ -55,7 +83,20 @@ class DViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Section-\(section)"
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
 
+        switch Section(rawValue: indexPath.section) {
+        case .some(.User):
+            print("User \(indexPath.row)")
+        case .some(.Account):
+            print("Account \(indexPath.row)")
+        default:
+            print("Default")
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
